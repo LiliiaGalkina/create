f52e8fa8301966d53515381cea6560cd6039ad46
Liliia Galkina <vvlili@yandex.ru>
2026-05-18 15:13:04 +0300

    abouthero с адаптивом

--- src/components/AboutHero/AboutHero.tsx
+++ src/components/AboutHero/AboutHero.tsx
@@ -7,25 +7,25 @@ import HeroTitleblock from "../HeroTitleBlock/HeroTitleblock";
 import style from "./abouthero.module.scss";
 
 const AboutHero = () => {
-    return (
-      <div className={style.hero}>
-        <div className="container">
-          <HeroPagesDecors
-            shapesClass="aboutDecorShapesUp"
-            linesLeftClass="aboutDecorLittleLinesLeft"
-            linesUpClass="aboutDecorLinesUp"
-            manyShapesClass="aboutDecorManyShapesDown"
-            linesrightClass="aboutDecorLittleLinesRight"
-          />
-          <HeroTitleblock>
-            <Crumbs name="About us" />
-            <HeroTitle title="About Us"/>
-            <HeroText text="Our agency has been successfully operating in digital marketing since 2012. We are proud to offer you quality and exemplary service. We help our clients get better results with our creative and data-driven approach to online advertising."/>
-          </HeroTitleblock>
-          <HeroCards/>
-        </div>
+  return (
+    <div className={style.hero}>
+      <HeroPagesDecors
+        shapesClass="aboutDecorShapesUp"
+        linesLeftClass="aboutDecorLittleLinesLeft"
+        linesUpClass="aboutDecorLinesUp"
+        manyShapesClass="aboutDecorManyShapesDown"
+        linesrightClass="aboutDecorLittleLinesRight"
+      />
+      <div className="container">
+        <HeroTitleblock>
+          <Crumbs name="About us" />
+          <HeroTitle title="About Us" />
+          <HeroText text="Our agency has been successfully operating in digital marketing since 2012. We are proud to offer you quality and exemplary service. We help our clients get better results with our creative and data-driven approach to online advertising." />
+        </HeroTitleblock>
+        <HeroCards />
       </div>
-    );
-}
- 
+    </div>
+  );
+};
+
 export default AboutHero;
