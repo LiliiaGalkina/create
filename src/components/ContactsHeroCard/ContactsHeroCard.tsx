8e5b1c6cefac71a49b536d33492757a37661e7d9
Liliia Galkina <vvlili@yandex.ru>
2026-05-27 14:24:21 +0300

    contactshero c адаптивом и разделителями карточек

--- src/components/ContactsHeroCard/ContactsHeroCard.tsx
+++ src/components/ContactsHeroCard/ContactsHeroCard.tsx
@@ -2,21 +2,31 @@ import type React from "react";
 import style from "./contactsherocard.module.scss";
 import type { IPropsContactsHeroCards } from "../../types";
 
-
-const ContactsHeroCard: React.FC<IPropsContactsHeroCards> = ({image, alt, title, text1, text2, link}) => {
+const ContactsHeroCard: React.FC<IPropsContactsHeroCards> = ({
+  id,
+  image,
+  alt,
+  title,
+  text1,
+  text2,
+  link,
+}) => {
   return (
     <div className={style.card}>
-      <img src={image} alt={alt}  className={style.image}/>
-      <div className={style.info}>
-        <h4 className={style.subtitle}>{title}</h4>
-        <span className={style.text}>{text1}</span>
-        {
-          text2 && <span className={style.text}>{text2}</span>
-        }
-        {
-          link && <a href="#" className={style.link}>{link}</a>
-        }
+      <div className={style.content}>
+        <img src={image} alt={alt} className={style.image} />
+        <div className={style.info}>
+          <h4 className={style.subtitle}>{title}</h4>
+          <span className={style.text}>{text1}</span>
+          {text2 && <span className={style.text}>{text2}</span>}
+          {link && (
+            <a href="#" className={style.link}>
+              {link}
+            </a>
+          )}
+        </div>
       </div>
+     { id < 4 && <div className={style.separator}></div>}
     </div>
   );
 };
